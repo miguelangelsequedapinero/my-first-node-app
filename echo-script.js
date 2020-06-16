f572238014f5c40b87480363ea76415b7095bcba
gaiasamadhi <64321211+gaiasamadhi@users.noreply.github.com>
2020-06-16 13:59:03 +0200

    Implement echo-script

--- echo-script.js
+++ echo-script.js
@@ -1,2 +1,18 @@
 const fs = require('fs');
 const path = require('path');
+
+const filePath = path.join(__dirname, 'products.sql');
+
+const textFileContent = fs.readFileSync(filePath, 'utf8');
+
+const textAsArray = textFileContent.split('\n');
+
+
+const printF = textAsArray.forEach((line) => {
+
+                const see = line.replace('insert into products (id, price, name, date) values (', '')
+                const seeMore  = see.replace(');','')
+
+                const  textAsArrayValues = seeMore.split(', ')
+                console.log(textAsArrayValues[2])
+              })
